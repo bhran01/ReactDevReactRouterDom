869503c925a92a233a50abb4d1c94fc3d625e203
subhransuO01 <subhransu.das001@outlook.com>
2024-04-04 10:33:08 +0530

    added all pages including layout and used outlet

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
+import './App.css';
+import Layout from "./pages/Layout";
 
 export function App() {
     return (
         <GrandParent>
             <Parent>
-                <Child path="" element={<Home/>}></Child>
-                <Child path="/login" element={<Login/>}></Child>
-                <Child path="/register" element={<Register/>}></Child>
-                <Child path="*" element={<NotFound/>}></Child>
+                <Child path="/" element={<Layout/>}>
+                    <Child index element={<Home/>}></Child>
+                    <Child path="login" element={<Login/>}></Child>
+                    <Child path="register" element={<Register/>}></Child>
+                    <Child path="*" element={<NotFound/>}></Child>
+                </Child>
             </Parent>
         </GrandParent>
     )
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import ReactDOM from 'react-dom/client';
 import  {App}  from './App';
 
 const root = ReactDOM.createRoot(document.querySelector('div.rootElement'));
-root.render(<App/>);
+root.render(<App />);
