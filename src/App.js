58d4b89780884f9a32ebc388ea2d14c3f6e1cdab
subhransuO01 <subhransu.das001@outlook.com>
2024-04-04 08:28:52 +0530

    code commited for dymanic url

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as GrandParent, Routes as Parent ,Route as Child} from "react-router-dom";
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+    return (
+        <GrandParent>
+            <Parent>
+                <Child path="" element={<Home/>}></Child>
+                <Child path="/login" element={<Login/>}></Child>
+                <Child path="/register" element={<Register/>}></Child>
+                <Child path="*" element={<NotFound/>}></Child>
+            </Parent>
+        </GrandParent>
+    )
 }
-
-export default App;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <React.Fragment>
+    <div>404</div>
+    <a href='/'>Go To Homepage</a>
+    </React.Fragment>
+  )
+}
